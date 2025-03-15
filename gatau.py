a1f94305f9a63ada9fc14fa4bd126bdd07f2f0de
sabiandika <sabiandika2007@gmail.com>
2025-03-15 23:53:39 +0700

    Update gatau.py

--- gatau.py
+++ gatau.py
@@ -1,3 +1,3 @@
 print("Hello World")
-print("sabiandika ganteng psycho dingin coool abiezzzzzz")\
-    print("kalo muncul auto tidur nyenyak")
+print("sabiandika ganteng psycho dingin coool abiezzzzzz")
+print("kalo muncul auto tidur nyenyak")
