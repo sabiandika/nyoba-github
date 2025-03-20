5d0a23ed18a41ca880993b52381a445bcdf8096c
sabiandika <sabiandika2007@gmail.com>
2025-03-20 22:57:06 +0700

    data sain dq lab

--- gatau.py
+++ gatau.py
@@ -9,3 +9,19 @@ print(set(text))
 
 # tuple untuk
 print(tuple(text))
+
+
+i = input()  # inisialisasi variable i yang memiliki nilai 10
+
+if (i == 10):  # pengecekan nilai i apakah sama dengan
+    print("ini adalah angka 10")  # jika TRUE maka akan mencetak kalimat ini
+
+
+i = 5
+
+if (i == 5):
+    print("ini angka 5")
+elif (i > 5):
+    print("angka ini lebih dari 5")
+else:
+    print("angka ini kurang dari 5")
