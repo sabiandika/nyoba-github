8cd2d0208c1b692dc43d52abe1c25e45ff4d5dfd
sabiandika <sabiandika2007@gmail.com>
2025-03-21 07:35:44 +0700

    looping for 1

--- gatau.py
+++ gatau.py
@@ -8,7 +8,8 @@ else:
     print("angka ini kurang dari 5")
 
 # perulangan while
- # nilai awal j = 0
+print("perulangan while")
+# nilai awal j = 0
 j = 0
 # ketika j kurang dari 6 lakukan perulangan, jika tidak stop perulangan
 while j < 6:
@@ -16,3 +17,10 @@ while j < 6:
     print("Ini adalah perulangan ke -", j)
     # setiap kali diakhir perulangan update nilai dengan ditambah 1
     j = j+1
+
+# perulangan for
+print("perulangan for 1")
+# perulangan for sebagai insisialisasi dari angka 1 hingga angka yang lebih kecil daripada 6.
+for i in range(1, 6):
+    # perintah jika looping akan tetap berjalan
+    print("ini adalah perulangan ke :", i)
