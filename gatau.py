81d870e97f6db69bfac30adf83622d46ba2fcae6
sabiandika <sabiandika2007@gmail.com>
2025-03-22 11:06:06 +0700

    fungsi  def dan return

--- gatau.py
+++ gatau.py
@@ -36,3 +36,33 @@ for i in range(1, 11):
 # kenapa ad dua dan nol pada if, karna (%2) berfungsi sebagai pembagi/modulus sedangkan (0) adalah sisa bagi.
 # cara kerja adalah apabila sisa bagi nya nol maka program akan stop pada if.
 # dan jika sisa bagi bukan nol maka program akan lanjut pada else. dan akan mengulang sebanyak yang diminta pada range.
+
+print("penggunaan def")
+# penggunaan def
+
+
+def luas_segitiga(alas, tinggi):  # alas dan tinggi merupakan parameter yang masuk
+    luas = (alas * tinggi) / 2
+    print("Luas segitiga: %f" % luas)
+    # fungsi %f disini untuk menetukan tipe bila #f akan menjadi float dan ini bisa diganti.
+    # apabila diganti %i maka akan menjadi integer dan %s akan menjadi string.
+
+
+# pemanggilan fungsi
+# 4 dan 6 merupakan parameter yang diinputkan kedalam fungsi luas segitiga fungsi luas segitiga
+luas_segitiga(4, 6)
+
+
+print("penggunaan def dan return")
+# penggunaan def dan return
+# alas dan tinggi merupakan parameter yang masuk
+
+
+def luas_segitiga(alas, tinggi):
+    luas = (alas*tinggi) / 2
+    return luas  # return digunakan untuk mengembalikan nilai ke fungsi yang memanggilnya
+
+
+# pemanggilan fungsi
+# 4 dam 6 merupakan parameter yang diinputkan kedalam fungsi luas segitiga
+print("Luas segitiga: %d" % luas_segitiga(4, 6))
