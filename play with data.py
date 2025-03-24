24a529dd5f832cf7c2b63f3f3d69cb0a0e3b7067
sabiandika <sabiandika2007@gmail.com>
2025-03-24 11:19:14 +0700

    BUAT BAR CHART PAKE LIBRARY PANDAS, MATPLOTLIB DAN NUMPY(HAPUS #)

--- play with data.py
+++ play with data.py
@@ -16,19 +16,24 @@ with closing(requests.get(url, stream=True)) as r:
     for row in reader:
         print(row)
 
-# BUAT BAR CHART PAKE LIBRARY PANDAS
+
+# BUAT BAR CHART PAKE LIBRARY PANDAS, MATPLOTLIB DAN NUMPY
 
 # import pandas as pd
 # import numpy as np  (harusnya gausah pake #, # disini fungsinya supaya import ini gak naik ke atas)
 # import matplotlib.pyplot as plt
 
-# [kalo mau pake library kaya panda , numpy, matplotlib, dll, harus diimport dulu/ download dulu baru bisa di run]
+# kalo mau pake library kaya panda , numpy, matplotlib, dll, harus diimport dulu/ download dulu baru bisa di run
 
-# table = pd.read_csv( "https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
+# table = pd.read_csv( "https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")  #{pd.read csv untuk membaca csv menggunakan pandas}
 # table.head()
-# x_label = table['NAMA KELURAHAN']
+# x_label = table['NAMA KELURAHAN']    #{x_label mengambil data dari tabel dari kolom 'NAMA KELURAHAN'}
 # plt.bar(x=np.arange(len(x_label)), height=table['LAKI-LAKI WNI'])
-# plt.show()
+# plt.xticks(np.arange(len(x_label)), table['NAMA KELURAHAN'], rotation=90)   #{buat ngambil NAMA KELURAHAN di bawah chart}
+# plt.xlabel('Kelurahan di Jakarta Pusat')     #{x sebelum label berfungsi menaruh label(perintahnya label) sesuai sumbu x}
+# plt.ylabel('Jumlah Penduduk Laki - Laki')     #{y sebelum label berfungsi menaruh label(perintahnya label) sesuai sumbu y}
+# plt.title('Persebaran Jumlah Penduduk Laki- Laki di Jakarta Pusat')    #{sedangkan label berfungsi menaruh label di atas chart}
 
+# plt.show() #{show berguna untuk menampilkan chart}
 
 # (harusnya gausah pake #, # disini fungsinya supaya import ini gak naik ke atas)
